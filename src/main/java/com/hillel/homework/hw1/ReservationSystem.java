ddd4828bdf9b0445258917fa6c3f8b63919541ce
MPerekhest <perekhrest3@gmail.com>
2024-06-17 05:42:45 +0200

    initial commit

---
+++ src/main/java/com/hillel/homework/hw1/ReservationSystem.java
@@ -0,0 +1,7 @@
+package com.hillel.homework.hw1;
+
+public class ReservationSystem {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/hillel/homework/hw1/Table.java
@@ -0,0 +1,4 @@
+package com.hillel.homework.hw1;
+
+public class Table {
+}
